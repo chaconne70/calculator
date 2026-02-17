9681b02044402c1f08fb3ada389f671a503594dc
chaconne70 <chaconne70@naver.com>
2026-02-17 17:51:13 +0900

    Add mod function in ctrl.py

--- ctrl.py
+++ ctrl.py
@@ -60,3 +60,12 @@ class Control:
         
         return pow(a, b)   
 
+    def mod(self, a, b):
+        try:
+            if(b==0):
+                raise Exception("Divisor Error")
+            
+        except Exception as e:
+            return e
+        
+        return a%b
